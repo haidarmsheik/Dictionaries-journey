275d5511dca9bb1c0f9c669e7a0675c189b9c02f
Haidar <haidarmshk@gmail.com>
2025-02-20 20:25:23 +0200

    Adding inputs

---
+++ Haidar.py
@@ -0,0 +1,6 @@
+salary = int(input ("please input your salary :") )
+month = input("please input the month : " )
+savingsPercentage = int(input("please enter the percentage of savings :" ) )
+rentPercentage = int(input("please enter the percentage of rent :") )
+electricityPercentage = int(input("please enter the percentage of electricity :") )
+
