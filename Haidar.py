73ccc72aae0d0b3982219f8e2c0444b8767bff64
Haidar <haidarmshk@gmail.com>
2025-02-20 20:39:10 +0200

    Adding the amount of savings , rent and electricity

--- Haidar.py
+++ Haidar.py
@@ -4,3 +4,6 @@ savingsPercentage = int(input("please enter the percentage of savings :" ) )
 rentPercentage = int(input("please enter the percentage of rent :") )
 electricityPercentage = int(input("please enter the percentage of electricity :") )
 
+savings = (savingsPercentage / 100) * salary
+rent = (rentPercentage / 100) * salary
+electricity = (electricityPercentage / 100) * salary
