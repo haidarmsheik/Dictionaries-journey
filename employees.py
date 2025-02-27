3dfd251df165b23b11f980e2c14578c00f2e01d3
Haidar <haidarmshk@gmail.com>
2025-02-27 13:50:58 +0200

    Adding company employees

---
+++ employees.py
@@ -0,0 +1,11 @@
+company_employees = {
+    "Engineering" : {
+        "Alice" :{"age" : 30 , "role" : "software engineer"},
+        "Bob" : {"age" : 28 , "role" : "DevOps Engineer"}
+    },
+    "Hr" : {
+        "Charlie" : {"age" : 35 , "role" : "HR Manager"}
+    }
+
+}
+print(company_employees)
